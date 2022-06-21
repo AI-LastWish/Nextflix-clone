98264f2090f1960577d41c5e89162ceef6413c73
Tri Nguyen <tri09t4@gmail.com>
2022-06-21 17:34:16 +0700

    HeaderItem component

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,21 @@
 import Image from 'next/image'
 import React from 'react'
+import HeaderItem from './HeaderItem'
+import {
+  HomeIcon
+} from '@heroicons/react/outline'
 
 const Header = () => {
   return (
     <header>
-      <div>This is the header</div>
-      <Image 
-        className='object-contain' 
-        src="https://upload.wikimedia.org/wikipedia/commons/6/69/Netflix_logo.svg" 
-        width={200} 
-        height={100} 
+      <div>
+        <HeaderItem title='HOME' Icon={HomeIcon} />
+      </div>
+      <Image
+        className='object-contain'
+        src="https://upload.wikimedia.org/wikipedia/commons/6/69/Netflix_logo.svg"
+        width={200}
+        height={100}
       />
     </header>
   )
